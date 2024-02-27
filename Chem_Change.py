b694cd85b1a3de3f3172545c89473a24f4a6235c
KewTo <110850772+KewTo@users.noreply.github.com>
2024-02-26 18:00:11 -0800

    Update Chem_Change.py

--- Chem_Change.py
+++ Chem_Change.py
@@ -1,159 +1,178 @@
+import pyautogui as auto
 import pygetwindow as gw
+import time
+import os
+from glob import glob
+from openpyxl import load_workbook
+import xlwings as xw
+import re
 
-WIINGS = input("Copy and paste the WIINGS order #: ")
-num_of_lines = int(input("How many lines are you changing?: "))
-
-while True:
-    try:
-        for _ in range(num_of_lines):
-            while True:
-                COAT_TOOL = input("Which tool are you changing: ")  # ACT01/CCT01/RCT03/RCT05/GCT01
-                COAT_TOOL = COAT_TOOL.upper()
-
-                if COAT_TOOL == 'ACT01' or COAT_TOOL == 'CCT01':
-                    CHEM_LINE = input("Which line are you changing: ")
-                    CHEM_LINE = CHEM_LINE.upper()
-                    if CHEM_LINE == 'ACT12' or CHEM_LINE == 'ACT13' or CHEM_LINE == 'C21' or CHEM_LINE == 'C21B':
-                        BOTTLE = 'EMX-7306'
-                    elif CHEM_LINE == 'ACT11' or CHEM_LINE == 'ACT14' or CHEM_LINE == 'C14':
-                        BOTTLE = 'EMX-7310'
-                    EXP_DATE = input("Input the expiration date: ")
-                    BOTTLE_TIME = input("Input time bottle was put on warming shelf: ")
-                    BATCH = input("Input the batch number: ")
-                    BATCH = BATCH.upper()
-                    BUDDY = input("Input your buddy name: ")
-                    BUDDY = BUDDY.upper()
-
-                    information = {
-                        'TOOL': COAT_TOOL,
-                        'CHEMICAL_LINE': CHEM_LINE,
-                        'BATCH_NUMBER': BATCH,
-                        'EXPIRATION_DATE': EXP_DATE,
-                        'WIINGS_NUMBER': WIINGS,
-                        'BUDDY_NAME': BUDDY,
-                        'BOTTLE_FROM_WARMING_SHELF': BOTTLE_TIME,
-                        'BOTTLE_NAME': BOTTLE
-                    }
-
-                    print(information)
-                    break
-
-                elif COAT_TOOL == 'RCT03' or COAT_TOOL == 'RCT05' or COAT_TOOL == 'GCT01':
-                    CHEM_LINE = input("Which line are you changing: ")
-                    CHEM_LINE = CHEM_LINE.upper()
-                    if CHEM_LINE == 'PCAR' or CHEM_LINE == 'PCARB':
-                        BOTTLE = 'FEP-171'
-                        viscosity = '3mPa*s'
-                    elif CHEM_LINE == '8K':
-                        BOTTLE = 'FEP-171'
-                        viscosity = '6mPa*s'
-                    elif CHEM_LINE == 'P53' or CHEM_LINE == 'P53B' or CHEM_LINE == 'P53C':
-                        BOTTLE = 'MES EP500JE 1.9X'
-                    elif CHEM_LINE == 'P37' or CHEM_LINE == 'P37B':
-                        BOTTLE = 'SEBP-9012'
-                    elif CHEM_LINE == 'P23':
-                        BOTTLE = 'OEBR-CAP130T4'
-                    elif CHEM_LINE == 'P81':
-                        BOTTLE = 'MES EP561JE-1.9cP'
-                    elif CHEM_LINE == 'P94' or CHEM_LINE == 'P94B':
-                        BOTTLE = 'SEBP-602F'
-                    elif CHEM_LINE == 'BARC':
-                        BOTTLE = 'E2 STACK AL9412-302'
-                    elif CHEM_LINE == 'N19' or CHEM_LINE == 'N19B':
-                        BOTTLE = 'SEBN-306F'
-                    else:
-                        BOTTLE = ''
-                    BATCH = input("Input the batch number: ")
-                    BATCH = BATCH.upper()
-                    EXP_DATE = input("Input the expiration date: ")
-                    BUDDY = input("Input your buddy name: ")
-                    BUDDY = BUDDY.upper()
-
-                    information = {
-                        'TOOL': COAT_TOOL,
-                        'CHEMICAL_LINE': CHEM_LINE,
-                        'BATCH_NUMBER': BATCH,
-                        'EXPIRATION_DATE': EXP_DATE,
-                        'WIINGS_NUMBER': WIINGS,
-                        'BUDDY_NAME': BUDDY,
-                        'BOTTLE_NAME': BOTTLE
-                    }
-
-                else:
-                    print('Invalid Tool; please enter a valid tool')
-
-    except ValueError:
-        print('Invalid Response: ')
-        continue
-
-    else:
-        break
+# Automated 9904 disposition list for COAT Module. This is intended to grab the excel file from the Intel website, apply
+# certain filters and formula in conjuncture with previous 9904 dispo list from the past. With the previous dispo list
+# and VLOOKUP, it will apply which BLANKs belong to which engineer. It will sort and color code names for easier view.
 
 
-def prompt_script():
-    TOOL, CHEMICAL_LINE, BATCH_NUMBER, EXPIRATION_DATE, WIINGS_NUMBER, BUDDY_NAME, BOTTLE_FROM_WARMING_SHELF, BOTTLE_NAME = tool_prompt().values()
-
-    LEVELS_TYPE = ['PARTIAL', 'NEAR EMPTY', 'EMPTY']
-
-    while True:
-        LEVELS = input("What was the level of the bottle you changed?: ")
-        LEVELS = LEVELS.upper()
-        if LEVELS in LEVELS_TYPE:
-            LEVELS = LEVELS
-            break
-        else:
-            print("Invalid chemical levels: Please use: " f"{LEVELS_TYPE}")
-
-    if num_of_lines > 1:
-        print(
-            f"WIINGS: {WIINGS_NUMBER} \n "
-            f"BUDDY: {BUDDY_NAME} \n "
-            f"\n "
-            f"\n "
-            f" {CHEMICAL_LINE} REPLACED {LEVELS} BOTTLE WITH BOTTLE FROM WARMING SHELF \n  "
-            f"\n "
-            f"{BOTTLE_NAME} \n "
-            f"BATCH: {BATCH_NUMBER} \n "
-            f"EXP DATE: {EXPIRATION_DATE} \n "
-            f"\n "
-            f"KT "
-        )
-
-    if TOOL == 'ACT01' or TOOL == 'CCT01':
-        print(
-            f" {CHEMICAL_LINE} REPLACED EMPTY BOTTLE WITH BOTTLE FROM WARMING SHELF \n  "
-            f"\n "
-            f"BOTTLE FROM WARMING SHELF: {BOTTLE_FROM_WARMING_SHELF} \n "
-            f"\n "
-            f"{BOTTLE_NAME} \n "
-            f"BATCH: {BATCH_NUMBER} \n "
-            f"EXP DATE: {EXPIRATION_DATE} \n "
-            f"WIINGS: {WIINGS_NUMBER} \n "
-            f"BUDDY: {BUDDY_NAME} \n "
-            f"\n "
-            f"KT "
-        )
-
-    elif TOOL == 'RCT03' or TOOL == 'RCT05' or TOOL == 'GCT01':
-        print(
-            f" {TOOL}-{CHEMICAL_LINE} REPLACED EMPTY BOTTLE WITH BOTTLE FROM FRIDGE \n  "
-            f"\n "
-            f"{BOTTLE_NAME} \n "
-            f"BATCH: {BATCH_NUMBER} \n "
-            f"EXP DATE: {EXPIRATION_DATE} \n "
-            f"WIINGS: {WIINGS_NUMBER} \n "
-            f"BUDDY: {BUDDY_NAME} \n "
-            f"\n "
-            f"KT "
-        )
-
-
-def get_Workstream():
-    win = gw.getWindowsWithTitle('EXTRA! X-treme')[0]
+def grab_file():
+    win = gw.getWindowsWithTitle('New Tab - Google Chrome')[0]
     win.activate()
+    auto.hotkey('ctrl', 't')
+    time.sleep(2)
+    auto.typewrite('https://imosc-ebiz.intel.com/imobi/module_stores.asp')
+    auto.press('enter')
+    time.sleep(30)
+    for index in range(15):
+        auto.press('tab')
+        time.sleep(0.1)
+        print(index)
+    for index in range(4):
+        auto.press('down')
+        time.sleep(0.1)
+        print(index)
+    time.sleep(2)
+    auto.click()
+    win1 = gw.getWindowsWithTitle('Search by Module stores - Google Chrome')[0]
+    win1.activate()
+    time.sleep(120)
+    for index in range(2):
+        auto.press('tab')
+        time.sleep(0.1)
+    auto.press('enter')
 
 
-tool_prompt()
+def recent_file():
+    # Find latest Excel file
+    filepath = r'C:\Users\kevinto\Downloads\Search by Module Stores*.xlsx'
+    latest_file = max(glob(filepath), key=os.path.getmtime)
+    return latest_file
+
+
+wb = load_workbook(recent_file())
+ws = wb.active
+ws.delete_cols(10)
+wb.save(recent_file())
+time.sleep(2)
+xw.Book(recent_file())
+Dispo_Dimensions = ws.dimensions
+
+
+def filter_headers():
+    # Find all header titles column number for filtering
+    for row in ws.iter_rows():
+        for cell in row:
+            if cell.value == 'ENG_LOT_OWNER':
+                ENG_LOT_OWNER1 = cell.coordinate
+    return ENG_LOT_OWNER1
+
+
+def headers(x: str):
+    header = []
+    for i in range(1, ws.max_column):
+        header.append(ws.cell(row=2, column=i).value)
+    index = header.index(x) + 1
+    return index
+
+
+def copy_info():
+    # Copy previous 9904 dispo list to a new sheet
+    info_workbook = sorted(glob(r'C:\Users\kevinto\Downloads\Search by Module Stores*.xlsx'), key=os.path.getmtime)[-2]
+    wb1 = xw.Book(info_workbook)  # Initial file with data
+    wb2 = xw.Book(recent_file())  # Target file
+    ws1 = wb1.sheets[0]  # [1]
+    ws1.api.Copy(After=wb2.sheets[0].api)
+    wb2.sheets[1].name = 'Sheet2'
+
+    # Change to Sheet1 and maximize excel window screen
+    time.sleep(1)
+    auto.press('F6')
+    auto.press('left')
+    auto.press('enter')
+    time.sleep(1)
+    auto.hotkey('win', 'up')
+    time.sleep(0.5)
+
+
+def vlookup():
+    # Apply VLOOKUP Formula on Status Column
+    for x in range(2, len(xw.sheets[0].range('K1' + ':K' + str(ws.max_row)).rows)):
+        xw.sheets[0].range('K' + str(xw.sheets[0].range('K1' + ':K' + str(ws.max_row))[x].row)).value = '=VLOOKUP(C' + str(
+            x + 1) + ', Sheet2' + '!' + 'A:F, 6, FALSE)'
+
+
+def copy_paste():
+    # Copy vlookup on Status to ENG_LOT_OWNER
+    head = []
+    for i in range(2, len(xw.sheets[0].range('K1' + ':K' + str(ws.max_row)).rows)):
+        if xw.sheets[0].range('K1' + ':K' + str(ws.max_row))[i].value is not None:
+            head.append('K' + str(xw.sheets[0].range('K1' + ':K' + str(ws.max_row))[i].row))
+    for i in head:
+        xw.sheets[0].range(i).value = xw.sheets[0].range(i).options(ndim=2).value
+        xw.sheets[0].range(i).copy()
+        xw.sheets[0].range('J' + str(i[1:])).paste()
+
+
+def owner_name():
+    # Color code ENG_LOT_OWNER Color = (R,G,B)
+    for index, elem in enumerate(xw.sheets[0].range('J1' + ':J' + str(ws.max_row)).value, start=1):
+        if re.match(r'^BWOLSON',  str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (204, 0, 0)
+        elif re.match(r'^GLUU', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (204, 102, 102)
+        elif re.match(r'^HJAVAID', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (204, 0, 204)
+        elif re.match(r'^IECHOLS', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (102, 204, 204)
+        elif re.match(r'^JABELARD', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (204, 0, 102)
+        elif re.match(r'^JKBOSWOR', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (204, 204, 0)
+        elif re.match(r'^JRNISKAL', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (0, 204, 0)
+        elif re.match(r'^PSRIVAS2', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (0, 204, 204)
+        elif re.match(r'^SCPRICE', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (0, 0, 204)
+        elif re.match(r'^YUNPINGF', str(elem), flags=re.IGNORECASE):
+            xw.Range(filter_headers()[0] + str(index)).color = (102, 0, 204)
+        elif elem == 'ENG_LOT_OWNER':
+            pass
+        else:
+            xw.Range(filter_headers()[0] + str(index)).color = (204, 204, 102)
+
+
+def delete_extra():
+    # Delete more GOLDEN_MASK without Y
+    for index, elem in reversed(list(enumerate(xw.sheets[0].range('C1' + ':C' + str(ws.max_row)).value, start=1))):
+        if elem == "BLNK408215": #PEB GOLDEN MASK MONITOR
+            xw.sheets[0].range('A' + str(index) + ':' + 'V' + str(index)).delete()
+        elif re.match(r'.*CUPWASH', str(elem)):
+            xw.sheets[0].range('A' + str(index) + ':' + 'V' + str(index)).delete()
+        elif re.match(r'.*WARM1', str(elem)):
+            xw.sheets[0].range('A' + str(index) + ':' + 'V' + str(index)).delete()
+    # Delete unnecessary rows/columns
+    xw.sheets[0].range('A1:V1').delete()
+    xw.sheets[0].range('I1:I' + str(xw.sheets[0].range(Dispo_Dimensions).current_region.last_cell.row)).delete()
+    xw.sheets[0].range('E1:E' + str(xw.sheets[0].range(Dispo_Dimensions).current_region.last_cell.row)).delete()
+    xw.sheets[0].range('B1:B' + str(xw.sheets[0].range(Dispo_Dimensions).current_region.last_cell.row)).delete()
+    xw.sheets[0].range('A1:A' + str(xw.sheets[0].range(Dispo_Dimensions).current_region.last_cell.row)).delete()
+
+
+def stall_out():
+    import time
+    start = time.time()
+    input("Press any key to continue:")
+    end = time.time()
+    elapsed = end-start
+    elapsed = elapsed / 60
+    print(str(elapsed) + " minutes has elapsed")
+
+
+grab_file()
+copy_info()
+vlookup()
+copy_paste()
+stall_out()
+owner_name()
+delete_extra()
 
 if __name__ == '__main__':
     pass
